e23bc8e8b17b8b4f7bc0f6e1937024a29681b4a2
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-04 17:51:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Leetcode 3.44 Reverse String
+//Write a function that reverses a string. The input string is given as an array of characters s.
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+   
+};
