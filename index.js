56b7c09a6e3537e1f521be4f3f52f13be5fccfb7
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-04 18:18:49 +0000

    first solution with the return

--- index.js
+++ index.js
@@ -8,5 +8,16 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-   
-};
+    let index1 = 0;
+    let index2 = s.length - 1;
+    while (index1 < index2){
+        let temp = s[index1];
+        s[index1] = s[index2];
+        s[index2] = temp;
+        index1++;
+        index2--;
+
+    }; 
+    return s;  //don't need return making runtime a bit slower with this return
+         
+}
